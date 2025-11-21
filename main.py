3330a030f6c16fe92c45c4bbac3a166b0471cf9e
akashrahman-me <akashrahman.me@gmail.com>
2025-11-21 15:49:13 +0600

    inital setup done

---
+++ main.py
@@ -0,0 +1,27 @@
+from json import JSONDecodeError
+
+import requests
+import csv
+from datetime import datetime, timezone
+
+username = "akash_codespace"
+
+url = f"https://www.fiverr.com/users/{username}/is_online"
+headers = {
+    "Referer": f"https://www.fiverr.com/{username}",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"
+}
+
+r = requests.get(url, headers=headers)
+status = "ERROR"
+
+try:
+    status = r.json()["is_online"]
+    status = "ONLINE" if status else "OFFLINE"
+except (JSONDecodeError, KeyError) as e:
+    pass
+
+
+with open("status.csv", "a", newline="") as f:
+    w = csv.writer(f)
+    w.writerow([datetime.now(timezone.utc).isoformat(), status])
